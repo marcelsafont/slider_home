cf85c62bf3bc1dc03184773120406633fecccbf5
unknown <Marcel Safont@id28549.ub.edu>
2012-12-18 15:30:53 +0100

    segona pujada

---
+++ js/init.js
@@ -0,0 +1,8 @@
+    $(document).ready(function(){    
+            // haciendo uso de la parametrización por defecto    
+            $("#slider").slider();  
+      
+            // asignando un color de fondo diferente  
+            // $("form :input:visible").highlight({background: 'red'});  
+      
+    });  
