a5a0281ac406af0685552f7dd11959d91ee7f3a1
marcel safont <marcel.safont1983@gmail.com>
2012-12-21 00:02:19 +0100

    revisio amb mes efectes. pendent efecte stop

--- js/jquery.plugin_slider.js
+++ js/jquery.plugin_slider.js
@@ -4,13 +4,26 @@ $.fn.slider_ub = function(){
 		$(this).css({'position': 'absolute', 'top':'0'});
 		
 	});
-	$(this).find('#thumbs li a').hover(function(){
-		var num = $(this).parent().index();
-		$('#banners li').hide();
-		$('#banners .caption').css('top','40px');
-		$('#banners li').eq(num).show();
-		$('#banners li').eq(num).find('.caption').css('top','-30px');
-	})
+	$(this).find('#thumbs li a').hover(
+		//hover
+		function(){
+			var num = $(this).parent().index();
+			$('#banners li').css('opacity', '0');
+			$('#banners .caption').css('top', '0px');
+			$('#banners li').eq(num).animate({opacity:1}, 1000);
+
+			
+			$('#banners li').eq(num).find('.caption').animate({
+				top:'-40px',
+				opacity: 1
+			}, 1000)
+		},
+		//out
+		function(){
+
+		}
+
+	)
 };
 
 
